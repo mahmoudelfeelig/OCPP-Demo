0ab258dd2081d68544f228a522f5567a5a3c8551
feel <mahmoudelfeelig@gmail.com>
2026-06-27 05:39:04 +0200

    fix environment variables for production deployment

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,9 +5,11 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from app.core.config import get_settings
 from app.models import Base  # noqa: E402
 
 config = context.config
+config.set_main_option("sqlalchemy.url", get_settings().database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
